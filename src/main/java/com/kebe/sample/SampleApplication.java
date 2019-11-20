102753276fb6dee5b36bc070274200c0dd0a7c0d
yeoseong yoon <1223yys@naver.com>
2019-11-20 23:22:57 +0900

    fix: kube-resource file modified

--- src/main/java/com/kebe/sample/SampleApplication.java
+++ src/main/java/com/kebe/sample/SampleApplication.java
@@ -2,20 +2,27 @@ package com.kebe.sample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
 
 @RestController
 @SpringBootApplication
 public class SampleApplication {
 
+    private int random = (int)(Math.random()*10)+1;
+
     public static void main(String[] args) {
         SpringApplication.run(SampleApplication.class, args);
     }
 
-    @RequestMapping("/")
-    public String index(){
-        return "Hello Kubernetes!";
+    @RequestMapping("/api")
+    public String api(){
+        return "new api ! - "+random;
     }
 
 }
