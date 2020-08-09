d6780508dcedc5bca4568e061c0de2c92b22965f
levi.yoon <levi.yoon@kakaocorp.com>
2020-08-09 23:31:14 +0900

    adding config map and apply config map to deployment manifast

--- src/main/java/com/kebe/sample/SampleApplication.java
+++ src/main/java/com/kebe/sample/SampleApplication.java
@@ -2,13 +2,8 @@ package com.kebe.sample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Random;
 
 @RestController
 @SpringBootApplication
@@ -24,5 +19,4 @@ public class SampleApplication {
     public String api(){
         return "new api ! - "+random;
     }
-
 }
