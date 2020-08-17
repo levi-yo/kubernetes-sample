755e5d8a7d4458ca1c95005f64b316a79e66317a
levi.yoon <levi.yoon@kakaocorp.com>
2020-08-17 17:57:12 +0900

    Adding logging to api

--- src/main/java/com/kebe/sample/SampleApplication.java
+++ src/main/java/com/kebe/sample/SampleApplication.java
@@ -1,10 +1,16 @@
 package com.kebe.sample;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
+@Slf4j
 @RestController
 @SpringBootApplication
 public class SampleApplication {
@@ -17,6 +23,7 @@ public class SampleApplication {
 
     @RequestMapping("/api")
     public String api(){
+        log.info("SampleApplication#api :::: current time = {}", LocalDateTime.ofInstant(Instant.now(), ZoneId.systemDefault()));
         return "new api ! - "+random;
     }
 }
